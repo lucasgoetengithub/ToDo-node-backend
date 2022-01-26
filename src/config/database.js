04eaf521d1d2ecfbcb69f143a79a26209cbaf1e6
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-01-26 00:03:42 -0300

    correção base mongo

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/todo';
 
-mongoose.connect(url, { useNewUrlParser:trye });
+mongoose.connect(url, { useNewUrlParser:true });
 
 module.exports = mongoose;
