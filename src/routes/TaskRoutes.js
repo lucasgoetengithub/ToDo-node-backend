30d90450fa71e5a979de0a64b3819958d7925abe
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-02-02 18:51:09 -0300

    listando todas tarefas

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
+const MacadressValidation = require('../middlewares/MacadressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskController.update);
+router.get('/filter/all', MacadressValidation, TaskController.all);
 
 module.exports = router;
