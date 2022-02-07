d513b6e42229eac31a20b8cfbdb20377644d9dfa
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-02-06 23:32:35 -0300

    atualizando status

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -48,6 +48,33 @@ class TaskController {
             return res.status(500).json(error);
         });
     }
+
+    async delete(req, res) {
+        await TaskModel.deleteOne({'_id': req.params.id})
+        .then(response => {
+            if (response) 
+                return res.status(200).json(response);
+            else
+                return res.status(404).json({error: 'tarefa não encontrada'});
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+    }
+
+    async done(req, res){
+        await TaskModel.findByIdAndUpdate(
+            {'_id': req.params.id},
+            {'done': req.params.done},
+            {new: true}
+            )
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+    }
 }
 
 module.exports = new TaskController();
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -9,7 +9,8 @@ const MacadressValidation = require('../middlewares/MacadressValidation');
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskController.update);
 router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
 router.get('/filter/all', MacadressValidation, TaskController.all);
-
+router.put('/:id/:done', TaskController.done);
 
 module.exports = router;
