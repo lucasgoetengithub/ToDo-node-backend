a689dd52ed0a9d8de1d8d5ab0f7fcbc50f7cd2d7
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-04-19 19:12:31 -0300

    atualização do backend

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -7,15 +7,16 @@ const TaskValidation = require('../middlewares/TaskValidation');
 const MacadressValidation = require('../middlewares/MacadressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
-router.put('/:id', TaskController.update);
+router.put('/:id', TaskValidation,  TaskController.update);
 router.get('/:id', TaskController.show);
 router.delete('/:id', TaskController.delete);
-router.get('/filter/all', MacadressValidation, TaskController.all);
 router.put('/:id/:done', TaskController.done);
-router.get('/filter/late', MacadressValidation, TaskController.late);
-router.get('/filter/today', MacadressValidation, TaskController.today);
-router.get('/filter/week', MacadressValidation, TaskController.week);
-router.get('/filter/month', MacadressValidation, TaskController.month);
-router.get('/filter/year', MacadressValidation, TaskController.year);
+
+router.get('/filter/all/:macaddress',   TaskController.all);
+router.get('/filter/late/:macaddress',  TaskController.late);
+router.get('/filter/today/:macaddress', TaskController.today);
+router.get('/filter/week/:macaddress',  TaskController.week);
+router.get('/filter/month/:macaddress', TaskController.month);
+router.get('/filter/year/:macaddress',  TaskController.year);
 
 module.exports = router;
