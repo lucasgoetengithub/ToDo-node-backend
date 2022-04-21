c4e1e5099f174795e6166825d9fa8a55527c5c3c
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-04-21 18:42:28 -0300

    ajuste na validação

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const cors = require('cors');
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes);
